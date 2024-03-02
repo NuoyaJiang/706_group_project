0c99c06cad45fa8f3c6665ac8c6e17abe13e7634
Merlinaphist <mjz201126@outlook.com>
2024-03-01 22:43:14 -0500

    Add introduction

--- Home.py
+++ Home.py
@@ -7,13 +7,15 @@ st.set_page_config(
     page_icon="🦠",
 )
 
+home_url = "https://706groupprojectlmn.streamlit.app/TB_Drug_Resistance"
 
 st.write("# Welcome to Group NLM!")
 
 # st.sidebar.success("Select a demo above.")
 
 st.markdown(
-    """
+    f"""
     Tuberculosis (TB) is an infectious disease caused by the bacterial pathogen _Mycobacterium tuberculosis_, which mainly affects the lungs of the patients. Annually, more than 1 million patients die of TB worldwide, making TB the second leading cause of death from a single infectious agent, following COVID-19 [[1](https://www.who.int/teams/global-tuberculosis-programme/tb-reports/global-tuberculosis-report-2023)].
+    In this project, we visualize [its rising trend in drug resistance]({home_url}/TB_Drug_Resistance), [the burden caused by drug resistance]({home_url}/TB_Burden), and [socioeconomic factors that may help us hamper the trend]({home_url}/TB_Correlation).
 """
 )
