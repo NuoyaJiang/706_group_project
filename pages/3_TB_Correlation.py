310575c6758b4ef01ace5084126cc7f2bc4198fe
Merlinaphist <mjz201126@outlook.com>
2024-03-02 16:21:06 -0500

    Update 3_TB_Correlation.py

--- pages/3_TB_Correlation.py
+++ pages/3_TB_Correlation.py
@@ -52,8 +52,8 @@ corrplot = alt.Chart(corr_mat).mark_bar().encode(
 
 st.altair_chart(corrplot, use_container_width=True)
 
-x_option = st.selectbox('Select X dimension', subset.columns[0:6])
-y_option = st.selectbox('Select Y dimension', subset.columns[6:])
+x_option = st.selectbox('Select X dimension', subset.columns[6:])
+y_option = st.selectbox('Select Y dimension', subset.columns[0:6])
 
 scatterplot = alt.Chart(subset).mark_point().encode(
     y=alt.Y(y_option, title=y_option),
