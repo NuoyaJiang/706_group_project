c66b297f1c2588c8c5c56f9a62fad1f5c07f7f00
Xinyu <0v0smiling@gmail.com>
2024-03-02 00:30:17 -0500

    Update 2_TB_Burden.py

--- pages/2_TB_Burden.py
+++ pages/2_TB_Burden.py
@@ -145,7 +145,7 @@ chart_top = alt.hconcat(chart_treatmentrate, chart_trend_rate).resolve_scale(col
 chart_bottom = alt.hconcat(chart_incidence, chart_trend_incident).resolve_scale(color='independent')
 chart_all = chart_top & chart_bottom
 
-st.altair_chart(chart_bottom, use_container_width=True)
+st.altair_chart(chart_incidence, use_container_width=True)
 
 
 countries_in_subset = df3["country"].unique()
