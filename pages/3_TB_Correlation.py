2ddf2ebf6fb1303e285f03d0fe7f2700ac27352e
Merlinaphist <mjz201126@outlook.com>
2024-03-02 15:26:15 -0500

    corr test

--- pages/3_TB_Correlation.py
+++ pages/3_TB_Correlation.py
@@ -1,7 +1,8 @@
 import altair as alt
 import pandas as pd
-# import numpy as np
 import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 st.set_page_config(
     page_title="TB Correlation",
@@ -19,11 +20,13 @@ subset = data[["e_rr_pct_new", "e_rr_pct_ret", 'c_new_tsr', 'c_tsr_resist',
                "exp_lab", "exp_mdrmgt","exp_orsrvy","exp_oth", 
                "exp_patsup","exp_prog","exp_staff"]]
 
-# corr_mat = np.corrcoef(subset)
+corr_mat = subset.corr().iloc[4:,0:4].reset_index()
+corr_mat = pd.melt(corr_mat, id_vars="index", value_name="Corr", var_name="TB Burden")
+corr_mat.columns = ['Expenditure', 'TB Burden', 'Corr']
 
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-fig, ax = plt.subplots()
-sns.heatmap(subset.corr().iloc[4:,0:4], ax=ax)
-st.write(fig)
+alt.Chart(subset).mark_bar().encode(
+    x=alt.X("Expenditure:O"),
+    color=alt.Color("Corr:Q"),
+    y=alt.Y("TB Burden:N"),
+    tooltip=["Corr"],
+)
