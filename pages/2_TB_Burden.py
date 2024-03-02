e31cc40e5532ca8101a0e6e4fbdc3648fabf3192
Xinyu <0v0smiling@gmail.com>
2024-03-02 00:31:06 -0500

    Update 2_TB_Burden.py

--- pages/2_TB_Burden.py
+++ pages/2_TB_Burden.py
@@ -129,7 +129,7 @@ chart_trend_rate = alt.Chart(subset).mark_line(point=True).encode(
 
 chart_trend_incident = alt.Chart(subset).mark_line(point=True).encode(
     x=alt.X('year:T'),
-    y=alt.Y("e_inc_num:Q", title= 'TB Incidences', scale=alt.Scale(domain=[subset['e_inc_num'].min()-20, subset['e_inc_num'].min()+20])),
+    y=alt.Y("e_inc_num:Q", title= 'TB Incidences'),#, scale=alt.Scale(domain=[subset['e_inc_num'].min()-20, subset['e_inc_num'].min()+20])),
     color=alt.Color('country:N'),
     tooltip=['year:T', alt.Tooltip("e_inc_num:Q", title="TB Incidence Rate")]
 ).transform_filter(
@@ -145,7 +145,7 @@ chart_top = alt.hconcat(chart_treatmentrate, chart_trend_rate).resolve_scale(col
 chart_bottom = alt.hconcat(chart_incidence, chart_trend_incident).resolve_scale(color='independent')
 chart_all = chart_top & chart_bottom
 
-st.altair_chart(chart_incidence, use_container_width=True)
+st.altair_chart(chart_trend_incident, use_container_width=True)
 
 
 countries_in_subset = df3["country"].unique()
