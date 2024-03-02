6eb1d18f3eb1883d93bf1c7c2eec09242158a522
Nuoya Jiang <nuoyajiang@g.ucla.edu>
2024-03-02 14:39:10 -0500

    fix df2

--- pages/1_TB_Drug_Resistance.py
+++ pages/1_TB_Drug_Resistance.py
@@ -92,7 +92,7 @@ chart_resistance = chart_base.mark_geoshape().encode(
 chart_resistance = alt.vconcat(background + chart_resistance).resolve_scale(color='independent')
 #st.altair_chart(chart_resistance, use_container_width=True)
 
-df2 = df[df["country"].isin(countries_options)]
+df2 = df[df["country"].isin(countries_options)][["country", "e_rr_pct_ret", "country-code"]]
 st.write(df2)
 
 chart_trend_rate = alt.Chart(df1).mark_line(point=True).encode(
