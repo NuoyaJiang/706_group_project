268b948876166caa49f1f6f3b9872d3f7c284251
Merlinaphist <mjz201126@outlook.com>
2024-03-01 22:53:38 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,4 +19,4 @@ st.markdown(
 """
 )
 
-st.markdown(f"<p>In this project, we visualize <a href={home_url+'/TB_Drug_Resistance'}>its rising trend in drug resistance</a>, [the burden caused by drug resistance]({home_url}/TB_Burden), and [socioeconomic factors that may help us hamper the trend]({home_url}/TB_Correlation).</p>")
+st.markdown(f"In this project, we visualize <a href={home_url+'/TB_Drug_Resistance'}>its rising trend in drug resistance</a>, [the burden caused by drug resistance]({home_url}/TB_Burden), and [socioeconomic factors that may help us hamper the trend]({home_url}/TB_Correlation).")
