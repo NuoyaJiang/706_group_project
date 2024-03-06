7ab4ddec647ddc99bbca342e9750ff0d5521b8f5
Merlinaphist <mjz201126@outlook.com>
2024-03-06 13:39:53 -0500

    Update 3_TB_Correlation.py

--- pages/3_TB_Correlation.py
+++ pages/3_TB_Correlation.py
@@ -56,12 +56,18 @@ st.altair_chart(corrplot, use_container_width=True)
 x_option = st.selectbox('Select X dimension', subset.columns[6:])
 y_option = st.selectbox('Select Y dimension', subset.columns[0:6])
 
+y = alt.Y(y_option, title=y_option, scale=alt.Scale(type=f"{'symlog' if y_option in subset.columns[4:6] else 'linear'}"))
+x = alt.X(x_option, title=x_option, scale=alt.Scale(type='sqrt'))
+
 scatterplot = alt.Chart(subset).mark_point().encode(
-    y=alt.Y(y_option, title=y_option, scale=alt.Scale(type=f"{'symlog' if y_option in subset.columns[4:6] else 'linear'}")),
-    x=alt.X(x_option, title=x_option, scale=alt.Scale(type='sqrt')),
+    y=y,
+    x=x,
 ).configure_axis(
         titleFontSize=14,
         labelLimit=0
 )
+
+scatterplot + scatterplot.transform_regression(x, y).mark_line()
+
 st.altair_chart(scatterplot, use_container_width=True)
 
